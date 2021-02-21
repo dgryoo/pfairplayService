0a9c23472a32ea2743cd44e6f32feb69baf65d7d
ryoo <dgryoo95@gmail.com>
2021-02-21 21:52:42 +0900

    modify field type, add length constraints

--- src/main/java/com/example/pfairplayservice/jpa/model/MemberEntity.java
+++ src/main/java/com/example/pfairplayservice/jpa/model/MemberEntity.java
@@ -8,11 +8,13 @@ import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.hibernate.validator.constraints.Length;
 
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.regex.PatternSyntaxException;
 
 @Builder
 @Data
@@ -29,30 +31,35 @@ public class MemberEntity {
     private String uid;
 
     @Column(nullable = false)
+    @Length(min = 6, max = 10)
     private String id;
 
     @Column(nullable = false)
+    @Length(min = 6, max = 10)
     private String password;
 
     @Column(nullable = false)
+    @Length(min = 2, max = 10)
     private String name;
 
-    @Column(nullable = false)
-    private Date birthday;
+    @Column(nullable = false, length = 8)
+    private Integer birthday;
 
     @Column(nullable = false)
     private String address;
 
-    @Column(nullable = false)
+    @Column(nullable = false, length = 11)
     private String phoneNumber;
 
-    @Column
+    @Column(length = 1)
     private Integer preferPosition;
 
-    @Column
+    @Column(length = 1)
+    @ColumnDefault("1")
     private Integer level;
 
-    @Column(nullable = false)
+
+    @Column(nullable = false, length = 1)
     @ColumnDefault("1")
     private Integer phoneNumberDisclosureOption;
 
