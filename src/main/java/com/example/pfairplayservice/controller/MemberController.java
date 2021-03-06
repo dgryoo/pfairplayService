930abf939b671b1a778cfca342e8d60402d6cbe9
ryoo <dgryoo95@gmail.com>
2021-03-07 01:02:18 +0900

    add indentation

--- src/main/java/com/example/pfairplayservice/controller/MemberController.java
+++ src/main/java/com/example/pfairplayservice/controller/MemberController.java
@@ -11,14 +11,7 @@ import com.example.pfairplayservice.model.Member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -45,7 +38,9 @@ public class MemberController {
     public ResponseEntity<Void> createMember(@RequestBody Member saveMember) {
         EntityExceptionHandler.MemberPostExceptionHandler(saveMember);
         Optional<MemberEntity> member = memberRepository.findByMemberId(saveMember.getId());
-        if (member.isPresent()) return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        if (member.isPresent()) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
         memberRepository.save(saveMember.toMemberEntity());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
