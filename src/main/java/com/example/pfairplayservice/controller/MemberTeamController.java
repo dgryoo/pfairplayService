aa8d55fc6f841eef0105c6f3ab787a0541eb52c9
ryoo <dgryoo95@gmail.com>
2021-04-26 22:23:21 +0900

    Create memberteam-delete api.

--- src/main/java/com/example/pfairplayservice/controller/MemberTeamController.java
+++ src/main/java/com/example/pfairplayservice/controller/MemberTeamController.java
@@ -10,6 +10,7 @@ import com.example.pfairplayservice.jpa.repository.TeamRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,6 +49,30 @@ public class MemberTeamController {
         memberTeamRepository.save(memberTeam);
 
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @DeleteMapping("/memberTeam")
+    public ResponseEntity<Void> deleteMemberTeam(@RequestBody MemberTeamEntity memberTeam) {
+
+        Optional<TeamEntity> teamEntity = teamRepository.findById(memberTeam.getMemberTeamId().getTid());
+        if (!teamEntity.isPresent()) {
+            throw new SourceNotFoundException(String.format("TID{%s} not found", memberTeam.getMemberTeamId().getTid()));
+        }
+
+        Optional<MemberEntity> memberEntity = memberRepository.findById(memberTeam.getMemberTeamId().getUid());
+        if (!memberEntity.isPresent()) {
+            throw new SourceNotFoundException(String.format("UID{%s} not found", memberTeam.getMemberTeamId().getUid()));
+        }
+
+        Optional<MemberTeamEntity> memberTeamEntity = memberTeamRepository.findById(memberTeam.getMemberTeamId());
+        if (!memberTeamEntity.isPresent()) {
+            throw new SourceNotFoundException("해당 팀의 멤버는 존재하지 않습니다.");
+        }
+
+        memberTeamRepository.delete(memberTeam);
+
+        return ResponseEntity.status(HttpStatus.OK).build();
+
     }
 
 }
