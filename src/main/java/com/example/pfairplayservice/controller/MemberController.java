b767b9bebfbc79549d642cc0159ca4e9f39a9b8d
ryoo <dgryoo95@gmail.com>
2021-05-10 18:17:13 +0900

    Modify by seperating member model

--- src/main/java/com/example/pfairplayservice/controller/MemberController.java
+++ src/main/java/com/example/pfairplayservice/controller/MemberController.java
@@ -7,8 +7,9 @@ import com.example.pfairplayservice.common.exception.SourceNotFoundException;
 import com.example.pfairplayservice.common.filter.FilterManager;
 import com.example.pfairplayservice.jpa.model.MemberEntity;
 import com.example.pfairplayservice.jpa.repository.MemberRepository;
-import com.example.pfairplayservice.model.modifier.MemberModifier;
-import com.example.pfairplayservice.model.origin.Member;
+import com.example.pfairplayservice.model.get.MemberForGet;
+import com.example.pfairplayservice.model.post.MemberForPost;
+import com.example.pfairplayservice.model.put.MemberForPut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,46 +26,46 @@ public class MemberController {
     private MemberRepository memberRepository;
 
     @GetMapping("/member/{uid}")
-    public ResponseEntity<Member> findByUid(@PathVariable String uid) {
-        Optional<MemberEntity> member = memberRepository.findById(uid);
+    public ResponseEntity<MemberForGet> findByUid(@PathVariable String uid) {
+        Optional<MemberEntity> memberEntity = memberRepository.findById(uid);
 
-        if (!member.isPresent()) {
+        if (!memberEntity.isPresent()) {
             throw new SourceNotFoundException(String.format("UID{%s} not found", uid));
         }
 
-        return ResponseEntity.status(HttpStatus.OK).body(Member.from(member.get()));
+        return ResponseEntity.status(HttpStatus.OK).body(MemberForGet.from(memberEntity.get()));
     }
 
     @PostMapping("/member")
-    public ResponseEntity<Void> createMember(@RequestBody Member member) {
-        EntityFieldValueChecker.checkMemberPostFieldValue(member);
-        Optional<MemberEntity> memberEntity = memberRepository.findByMemberId(member.getId());
+    public ResponseEntity<Void> createMember(@RequestBody MemberForPost memberForPost) {
+        EntityFieldValueChecker.checkMemberPostFieldValue(memberForPost);
+        Optional<MemberEntity> memberEntity = memberRepository.findByMemberId(memberForPost.getId());
         if (memberEntity.isPresent()) {
             return ResponseEntity.status(HttpStatus.CONFLICT).build();
         }
-        memberRepository.save(member.toMemberEntity());
+        memberRepository.save(memberForPost.toMemberEntity());
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @PutMapping("/member/{uid}")
-    public ResponseEntity<Void> updateByUid(@PathVariable String uid, @RequestBody MemberModifier memberModifier) {
+    public ResponseEntity<Void> updateByUid(@PathVariable String uid, @RequestBody MemberForPut memberForPut) {
         Optional<MemberEntity> memberEntity = memberRepository.findById(uid);
         if (!memberEntity.isPresent()) {
             throw new SourceNotFoundException(String.format("uid{%s} not found", uid));
         }
 
-        EntityFieldValueChecker.checkMemberPutFieldValue(memberModifier);
+        EntityFieldValueChecker.checkMemberPutFieldValue(memberForPut);
 
-        if (memberEntity.get().getAddress() != memberModifier.getAddress())
-            memberRepository.updateAddressByUid(uid, memberModifier.getAddress());
-        if (memberEntity.get().getPhoneNumber() != memberModifier.getAddress())
-            memberRepository.updatePhoneNumberByUid(uid, memberModifier.getPhoneNumber());
-        if (memberEntity.get().getPreferPosition() != memberModifier.getPreferPosition().getPosition())
-            memberRepository.updatePreferPositionByUid(uid, memberModifier.getPreferPosition().getPosition());
-        if (memberEntity.get().getLevel() != memberModifier.getLevel())
-            memberRepository.updateLevelByUid(uid, memberModifier.getLevel());
-        if (memberEntity.get().getPhoneNumberDisclosureOption() != memberModifier.getPhoneNumberDisclosureOption().getDisclosureOption())
-            memberRepository.updatePhoneNumberDisclosureOptionByUid(uid, memberModifier.getPhoneNumberDisclosureOption().getDisclosureOption());
+        if (memberEntity.get().getAddress() != memberForPut.getAddress())
+            memberRepository.updateAddressByUid(uid, memberForPut.getAddress());
+        if (memberEntity.get().getPhoneNumber() != memberForPut.getAddress())
+            memberRepository.updatePhoneNumberByUid(uid, memberForPut.getPhoneNumber());
+        if (memberEntity.get().getPreferPosition() != memberForPut.getPreferPosition().getPosition())
+            memberRepository.updatePreferPositionByUid(uid, memberForPut.getPreferPosition().getPosition());
+        if (memberEntity.get().getLevel() != memberForPut.getLevel())
+            memberRepository.updateLevelByUid(uid, memberForPut.getLevel());
+        if (memberEntity.get().getPhoneNumberDisclosureOption() != memberForPut.getPhoneNumberDisclosureOption().getDisclosureOption())
+            memberRepository.updatePhoneNumberDisclosureOptionByUid(uid, memberForPut.getPhoneNumberDisclosureOption().getDisclosureOption());
 
         return ResponseEntity.status(HttpStatus.OK).build();
     }
@@ -82,13 +83,13 @@ public class MemberController {
     }
 
     @GetMapping("/member/team/{tid}")
-    public ResponseEntity<List<Member>> findMemberListByTid(@PathVariable String tid) {
+    public ResponseEntity<List<MemberForGet>> findMemberListByTid(@PathVariable String tid) {
         List<MemberEntity> memberEntityList = memberRepository.findByMemberTeamIdTid(tid);
 
         if (memberEntityList == null)
             new SourceNotFoundException(String.format("member not found registered in tid{%s})", tid));
 
-        List<Member> memberList = memberEntityList.stream().map(FilterManager::teamMemberFilter).map(Member::from).collect(Collectors.toList());
+        List<MemberForGet> memberList = memberEntityList.stream().map(FilterManager::teamMemberFilter).map(MemberForGet::from).collect(Collectors.toList());
 
         return ResponseEntity.status(HttpStatus.OK).body(memberList);
     }
