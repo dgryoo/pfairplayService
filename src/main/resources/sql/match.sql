522cf898a4d393207d4baf758c4c22fee95ad2bb
ryoo <dgryoo95@gmail.com>
2021-06-06 00:35:47 +0900

    Modify create table query

--- src/main/resources/sql/match.sql
+++ src/main/resources/sql/match.sql
@@ -2,8 +2,8 @@ create table soccer_match (
                               match_no int primary key,
                               ground_number int not null,
                               price int not null,
-                              owner_team varchar(255) not null,
-                              guest_team varchar(255),
+                              owner_team_tid varchar(255) not null,
+                              guest_team_tid varchar(255),
                               start_date date not null,
                               end_date date not null,
                               message varchar(255),
@@ -11,6 +11,6 @@ create table soccer_match (
                               modified_date timestamp,
                               view_count int not null,
                               status int not null,
-                              foreign key (owner_team) references team(tid) on delete cascade,
-                              foreign key (guest_team) references team(tid)
+                              foreign key (owner_team_tid) references team(tid) on delete cascade,
+                              foreign key (guest_team_tid) references team(tid)
 ) engine=InnoDB default charset=utf8;
--- src/main/resources/sql/offer.sql
+++ src/main/resources/sql/offer.sql
@@ -1,12 +1,12 @@
 create table offer (
                        offer_no int primary key,
-                       target_match int not null,
-                       sand_team varchar(255) not null,
-                       receive_team varchar(255) not null,
+                       target_match_match_no int not null,
+                       sand_team_tid varchar(255) not null,
+                       receive_team_tid varchar(255) not null,
                        message varchar(255),
                        offer_date timestamp not null,
                        offer_status int not null,
-                       foreign key (target_match) references soccer_match(match_no) on delete cascade,
-                       foreign key (sand_team) references team(tid) on delete cascade,
-                       foreign key (receive_team) references team(tid) on delete cascade
+                       foreign key (target_match_match_no) references soccer_match(match_no) on delete cascade,
+                       foreign key (sand_team_tid) references team(tid) on delete cascade,
+                       foreign key (receive_team_tid) references team(tid) on delete cascade
 ) engine=InnoDB default charset=utf8;
