3572ec4d6fc4a9cb5f7488a766ce392f280ec389
ryoo <dgryoo95@gmail.com>
2021-06-24 00:34:07 +0900

    Modify entity creating sql

--- src/main/resources/sql/team.sql
+++ src/main/resources/sql/team.sql
@@ -2,6 +2,7 @@ create table team (
                       tid varchar(255) primary key,
                       team_name varchar(10) not null ,
                       team_lead_member_uid varchar(255) not null,
+                      level int not null,
                       activity_area_address varchar(255) not null,
                       registration_date date not null,
                       found_date date not null,
