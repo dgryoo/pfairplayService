08668e663b81115354675b1de23f56d0fad3fd34
ryoo <dgryoo95@gmail.com>
2021-08-04 01:47:36 +0900

    Modify findTeamReviewCounterByTidAndReviewIdList param String to List<String> and Query

--- src/main/java/com/example/pfairplayservice/cassandra/repository/TeamReviewCounterRepository.java
+++ src/main/java/com/example/pfairplayservice/cassandra/repository/TeamReviewCounterRepository.java
@@ -6,11 +6,15 @@ import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.data.cassandra.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamReviewCounterRepository extends CassandraRepository<TeamReviewCounter, TeamReviewCounterPrimaryKey> {
 
-    @Query(value = "select * from team_review_counter where tid = ?0 and review_id in (?1)")
-    List<TeamReviewCounter> findTeamReviewCounterByTid(String tid, String reviewIdCondition);
+    @Query(value = "select * from team_review_counter where tid = ?0 and review_id in ?1")
+    List<TeamReviewCounter> findTeamReviewCounterByTidAndReviewIdList(String tid, List<String> reviewIdCondition);
+
+    @Query(value = "select * from team_review_counter where tid = ?0 and review_id = ?1")
+    Optional<TeamReviewCounter> findTeamReviewCounterByTidAndReviewId(String tid, String reviewId);
 
     @Query(value = "update team_review_counter set thumbs_up_count = thumbs_up_count + 1 where tid = ?0 and review_id = ?1")
     void increaseThumbsUpByTidAndReviewId(String tid, String reviewId);
